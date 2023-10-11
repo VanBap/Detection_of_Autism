7ccf06f3b23267a4a1a5a217b4ee6d2e41e71554
VanBap <vanbap20021599@gmail.com>
2023-10-11 17:06:18 +0700

    3rd commit

--- app/src/main/java/com/example/detection_of_autism/MainActivity2.java
+++ app/src/main/java/com/example/detection_of_autism/MainActivity2.java
@@ -13,7 +13,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.detection_of_autism.ml.LiteModel;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -43,6 +50,38 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
 
+        predictBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                img = Bitmap.createScaledBitmap(img, 128, 128, true);
+                try {
+                    LiteModel model = LiteModel.newInstance(getApplicationContext());
+
+                    // Creates inputs for reference.
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 150, 150, 3}, DataType.FLOAT32);
+                    TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
+                    tensorImage.load(img);
+
+                    ByteBuffer byteBuffer = tensorImage.getBuffer();
+
+
+                    inputFeature0.loadBuffer(byteBuffer);
+
+                    // Runs model inference and gets result.
+                    LiteModel.Outputs outputs = model.process(inputFeature0);
+                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+                    // Releases model resources if no longer used.
+                    model.close();
+
+                    resView.setText(outputFeature0.getFloatArray()[0] + "\n" + outputFeature0.getFloatArray()[1]);
+                } catch (IOException e) {
+                    // TODO Handle the exception
+                }
+
+            }
+        });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
