42354008ff49d94500bbc17e5f3dae37826b4104
VanBap <vanbap20021599@gmail.com>
2023-10-16 22:53:51 +0700

    7th commit: add video recording

--- app/src/main/java/com/example/detection_of_autism/MainActivity2.java
+++ app/src/main/java/com/example/detection_of_autism/MainActivity2.java
@@ -16,7 +16,9 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.example.detection_of_autism.ml.LiteModel;
 
@@ -32,10 +34,12 @@ public class MainActivity2 extends AppCompatActivity {
     private Button selectBtn;
     private Button predictBtn;
     private Button captureBtn;
+    private Button recordBtn;
     private TextView resView;
     private ImageView imageView;
+    private VideoView videoView;
     private Bitmap bitmap;
-
+    int REQUEST_CODE_VIDEO_CAPTURE = 2607;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +51,16 @@ public class MainActivity2 extends AppCompatActivity {
         selectBtn = findViewById(R.id.selectBtn);
         predictBtn = (Button) findViewById(R.id.predictBtn);
         captureBtn = (Button) findViewById(R.id.captureBtn);
+        recordBtn = (Button) findViewById(R.id.recordBtn);
         resView = (TextView) findViewById(R.id.resView);
         imageView = (ImageView) findViewById(R.id.imageView);
+        videoView = (VideoView) findViewById(R.id.videoView);
 
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        //SELECT button
         selectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,11 +69,22 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivityForResult(intent, 10);
             }
         });
+
+        //CAPTURE button
         captureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent, 12);
+            }
+        });
+
+        //RECORD button
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivityForResult(intent, 13);
             }
         });
 
@@ -76,18 +98,25 @@ public class MainActivity2 extends AppCompatActivity {
 
                     // Creates inputs for reference.
                     TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 150, 150, 3}, DataType.FLOAT32);
-                    TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
-                    tensorImage.load(bitmap);
+                // ==========================================================================================
+                    //TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
+                    //tensorImage.load(bitmap);
 
-                    ByteBuffer byteBuffer = tensorImage.getBuffer();
+                    //ByteBuffer byteBuffer = tensorImage.getBuffer();
 
 
-                    inputFeature0.loadBuffer(byteBuffer);
+                    //inputFeature0.loadBuffer(byteBuffer);
 
                     // Runs model inference and gets result.
-                    LiteModel.Outputs outputs = model.process(inputFeature0);
-                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+                    //LiteModel.Outputs outputs = model.process(inputFeature0);
+                    //TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+                // ==========================================================================================
 
+                    bitmap = Bitmap.createScaledBitmap(bitmap, 128, 128, true);
+                    inputFeature0.loadBuffer(TensorImage.fromBitmap(bitmap).getBuffer());
+                    
+                    // Runs model inference and gets result.
+                    LiteModel.Outputs outputs = model.process(inputFeature0);
                     // Đang thắc mắc sẽ trả kết quả hiển thị kiểu gì ?
                     resView.setText("Van dep trai");
 
@@ -102,6 +131,8 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
     }
+
+    // permession
     void getPermission(){
         //if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M) {
 
@@ -122,6 +153,14 @@ public class MainActivity2 extends AppCompatActivity {
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
+    public void recordVideo(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_CODE_VIDEO_CAPTURE);
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
@@ -140,6 +179,11 @@ public class MainActivity2 extends AppCompatActivity {
         else if (requestCode == 12){
             bitmap = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(bitmap);
+        }
+        else if (requestCode == REQUEST_CODE_VIDEO_CAPTURE && resultCode == RESULT_OK){
+            Uri videoUri = data.getData();
+            videoView.setVideoURI(videoUri);
+            videoView.start();
         }
         super.onActivityResult(requestCode, resultCode, data);
 
